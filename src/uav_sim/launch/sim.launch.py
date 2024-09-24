9258007c0cd9b5dd7cba3b52044491a36281d378
Conan Dewitt <conanpodewitt@gmail.com>
2024-09-24 21:50:58 +0800

    fixed gazebo

--- src/uav_sim/launch/sim.launch.py
+++ src/uav_sim/launch/sim.launch.py
@@ -1,5 +1,4 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
@@ -9,36 +8,43 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    pkg_ros_gz_sim_demos = get_package_share_directory('uav_sim')
+    # Get paths for package directories
+    pkg_ros_gz_sim_uav_sim = get_package_share_directory('uav_sim')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    world_path = os.path.join(pkg_ros_gz_sim_demos, 'sdf', 'world.sdf')
-    uav_path = os.path.join(pkg_ros_gz_sim_demos, 'urdf', 'pioneer.urdf')
+    # Define paths for meshes, SDF world file, and URDF UAV model
+    meshes_path = os.path.join(pkg_ros_gz_sim_uav_sim, 'meshes')
+    world_path = os.path.join(pkg_ros_gz_sim_uav_sim, 'sdf', 'world.sdf')
+    uav_path = os.path.join(pkg_ros_gz_sim_uav_sim, 'urdf', 'pioneer.urdf')
 
+    # Read world description from the SDF file
     with open(world_path, 'r') as infp:
         world_desc = infp.read()
 
+    # Read UAV model description from the URDF file and replace mesh path placeholder
     with open(uav_path, 'r') as infp:
         uav_desc = infp.read()
+    uav_desc = uav_desc.replace('MESH_PATH', meshes_path)
 
+    # Declare launch argument for RViz option
     rviz_launch_arg = DeclareLaunchArgument(
         'rviz', default_value='true',
-        description='launch rviz'
+        description='Launch RViz'
     )
 
+    # Include Gazebo simulation launch file and specify the world SDF file
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'),
         ),
         launch_arguments={'gz_args': PathJoinSubstitution([
-            pkg_ros_gz_sim_demos,
+            pkg_ros_gz_sim_uav_sim,
             'sdf',
             'world.sdf'
         ])}.items(),
     )
 
-    # Get the parser plugin convert sdf to urdf using robot_description topic
+    # Node for publishing the robot's state using robot_state_publisher
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -46,44 +52,46 @@ def generate_launch_description():
         output='both',
         parameters=[
             {'use_sim_time': True},
-            {'robot_description': uav_desc},
+            {'robot_description': uav_desc},  # Send the UAV description to the robot_description topic
         ]
     )
 
-    # Launch rviz
+    # Node to launch RViz if specified in the launch argument
     rviz = Node(
         package='rviz2',
         executable='rviz2',
-        # arguments=['-d', os.path.join(pkg_ros_gz_sim_demos, 'rviz', 'vehicle.rviz')],
         condition=IfCondition(LaunchConfiguration('rviz')),
         parameters=[
             {'use_sim_time': True},
         ]
     )
 
-    robot = ExecuteProcess(
+    # Execute process to spawn the UAV into the simulation environment
+    uav = ExecuteProcess(
         cmd=['ros2', 'run', 'ros_gz_sim', 'create', '-topic', 'robot_description', '-z', '0.2'],
-        name='spawn rouavot',
+        name='spawn_uav',
         output='both'
     )
 
+    # Node to publish joint states for the robot
     joint_state_pub = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher'
     )
 
-    # A gui tool for easy tele-operation.
+    # Node to launch a GUI tool for tele-operating the robot
     robot_steering = Node(
         package='rqt_robot_steering',
         executable='rqt_robot_steering',
     )
 
+    # Return LaunchDescription object containing all the defined launch entities
     return LaunchDescription([
-        rviz_launch_arg,
-        gazebo,
-        robot,
-        robot_state_publisher,
-        joint_state_pub,
-        rviz,
-        robot_steering
+        rviz_launch_arg,        # Argument to enable RViz
+        gazebo,                 # Gazebo simulator with specified world
+        uav,                    # Process to spawn the UAV into Gazebo
+        robot_state_publisher,  # Publishes robot states
+        joint_state_pub,        # Publishes joint states for robot
+        rviz,                   # RViz for visualization
+        robot_steering          # GUI tool for controlling the robot
     ])
